aa64807ee0b1caac58644c3dbaf7f13a31d94521
Lorenzo Delana <lorenzo.delana@gmail.com>
2015-08-29 16:57:48 +0200

    added gitignore visual studio hooks. added mono managed wrapper for ArduiPi_OLED/GFX library (see README.mono.md). added example mono managed test program that display the clock.

---
+++ README.mono.md
@@ -0,0 +1,19 @@
+# ArduiPi_OLED Managed wrapper
+
+## Prerequisites
+
+```
+apt-get install mono-complete
+```
+
+## Build
+
+```
+./mono/build.sh
+```
+
+## Run test
+
+```
+./mono/run_test.sh
+```
---
+++ mono/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OLDPWD=`pwd`
+
+BD=`dirname $0`
+BASEDIR=`realpath $BD`
+
+echo
+echo "---> Build native library"
+
+cd $BASEDIR/..
+make
+
+echo
+echo "---> Build managed library"
+
+cd $BASEDIR
+echo "from `pwd`"
+xbuild /nologo /verbosity:quiet /toolsversion:4.0 ArduiPi_OLED_Wrapper.sln
+
+echo
+echo "---> type $BASEDIR/run_test.sh"
+
+cd $OLDPWD
+
